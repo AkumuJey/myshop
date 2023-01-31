52dd03b4135d6ce6d0fccefe7279bf4a3f4d11e6
AkumuJey <josephowino7@gmail.com>
2023-01-31 21:46:01 +0300

    nuxt routes

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla sed ad natus, repudiandae provident temporibus recusandae similique ratione perferendis ab deserunt modi quo possimus adipisci! Maxime illum fugiat autem. Assumenda numquam id distinctio repudiandae unde maxime asperiores consectetur odit?</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+h1 {
+    background: lightblue;
+    padding: 5px;
+}
+p{
+    margin: 10px;
+}
+</style>
